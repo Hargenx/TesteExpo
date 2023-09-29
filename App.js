83ed40bf98611101ccb5c993d78257a7c325f6f1
Raphael <Hargenx@hotmail.com>
2023-09-29 16:18:54 -0300

    weee

--- App.js
+++ App.js
@@ -1,11 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-export default function App() {
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = () => {
+    if (username === '') {
+      showErrorAlert('Erro', 'Por favor, insira um nome de usuário.');
+      return;
+    }
+
+    if (password === '') {
+      showErrorAlert('Erro', 'Por favor, insira uma senha.');
+      return;
+    }
+
+    const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+    if (!isEmailValid) {
+      showErrorAlert('Erro', 'Por favor, insira um e-mail válido.');
+      return;
+    }
+
+    console.log(`Username: ${username}, Password: ${password}, Email: ${email}`);
+  };
+
+  const showErrorAlert = (title, message) => {
+    Alert.alert(
+      title,
+      message,
+      [{ text: 'OK', onPress: () => console.log('OK Pressed') }],
+      { cancelable: false }
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Usuário"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="E-mail"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+      />
+      <Button
+        title="Entrar"
+        onPress={handleSubmit}
+        style={styles.button}
+        color="#007AFF"
+      />
     </View>
   );
 }
@@ -13,8 +73,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
+    textAlign: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: '#000',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 8,
+  },
+  button: {
+    textAlign: 'center',
   },
 });
+
+export default Login;
